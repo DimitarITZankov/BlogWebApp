85227597a1edab655f7b879fded77fc7e5954648
Mitko <dimitarzankovit@gmail.com>
2025-10-16 10:56:33 +0300

    Adding base.html and Main Page

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from flask import Flask  
+from flask import Flask,render_template 
+
+
+
+
+#Create The App
+app = Flask(__name__)
+
+
+
+#Create The Main Page
+@app.route('/')
+def index():
+    return render_template('index.html')
